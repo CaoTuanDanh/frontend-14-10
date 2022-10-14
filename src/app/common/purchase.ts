8e83e663351c1d5dbd230fae8f962573dc1c8586
Cao Tuan Danh <caotuandanh2103@gmail.com>
2022-10-14 13:39:42 +0700

    2.0

---
+++ src/app/common/purchase.ts
@@ -0,0 +1,7 @@
+export class Purchase {
+  customer: any;
+  shippingAddress: any;
+  billingAddress: any;
+  order: import("d:/website-datn/03-frontend/angular-ecommerce/src/app/common/order").Order;
+  orderItems: import("d:/website-datn/03-frontend/angular-ecommerce/src/app/common/order-item").OrderItem[];
+}
---
+++ src/app/validators/dtshop-validators.spec.ts
@@ -0,0 +1,7 @@
+import { DTShopValidators } from './dtshop-validators';
+
+describe('DTShopValidators', () => {
+  it('should create an instance', () => {
+    expect(new DTShopValidators()).toBeTruthy();
+  });
+});
